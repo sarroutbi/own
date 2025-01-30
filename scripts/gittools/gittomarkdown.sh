c164904668bbda8eac1b847df3f321854f6404c5
Sergio Arroutbi <sarroutb@redhat.com>
2025-01-30 09:15:51 +0100

    Fix license headers (#95)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/gittools/gittomarkdown.sh
+++ scripts/gittools/gittomarkdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023, Sergio Arroutbi Braojos <sarroutbi (at) redhat.com>
+# Copyright (c) 2023, Sergio Arroutbi Braojos <sarroutb (at) redhat.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
