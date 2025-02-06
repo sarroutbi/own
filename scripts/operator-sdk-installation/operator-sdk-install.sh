583201db0ae0151fab5653dbc90cbe5f96b6ed24
Sergio Arroutbi <sarroutb@redhat.com>
2025-02-06 09:37:16 +0100

    Parameterize operator-sdk-install.sh version
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/operator-sdk-installation/operator-sdk-install.sh
+++ scripts/operator-sdk-installation/operator-sdk-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Copyright (c) 2025 Red Hat, Inc.
 #
 # Author: Sergio Arroutbi <sarroutb@redhat.com>
@@ -16,14 +17,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
+DEFAULT_VERSION="1.39.1"
+
+function usage() {
+  echo "$1 [-v version]"
+  echo ""
+  exit "$2"
+}
+
+while getopts "v:h" arg
+do
+  case "${arg}" in
+    v) VERSION=${OPTARG}
+       ;;
+    h) usage "$0" 0 || return 0
+       ;;
+    *) usage "$0" 1 || return 1
+       ;;
+  esac
+done
+
+test -z "${VERSION}" && VERSION="${DEFAULT_VERSION}"
+echo "Downloading VERSION:${VERSION}"
+
 export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 export OS=$(uname | awk '{print tolower($0)}')
-export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.39.1
+export OPERATOR_SDK_DL_URL="https://github.com/operator-framework/operator-sdk/releases/download/v${VERSION}"
 curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
 gpg --keyserver keyserver.ubuntu.com --recv-keys 052996E2A20B5C7E
 curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt
 curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt.asc
 gpg -u "Operator SDK (release) <cncf-operator-sdk@cncf.io>" --verify checksums.txt.asc
 grep operator-sdk_${OS}_${ARCH} checksums.txt | sha256sum -c -
-chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+#chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
