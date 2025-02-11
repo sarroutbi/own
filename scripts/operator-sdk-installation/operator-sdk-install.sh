5dfd07c741939c1187fc5e7abe323c374f7c5eb1
Sergio Arroutbi <sarroutb@redhat.com>
2025-02-11 10:17:18 +0100

    Fix errors reported by shellcheck (#104)
    Signed-off-by: Sergio Arroutbi <sarroutbi@gmail.com>

--- scripts/operator-sdk-installation/operator-sdk-install.sh
+++ scripts/operator-sdk-installation/operator-sdk-install.sh
@@ -30,9 +30,9 @@ do
   case "${arg}" in
     v) VERSION=${OPTARG}
        ;;
-    h) usage "$0" 0 || return 0
+    h) usage "$0" 0
        ;;
-    *) usage "$0" 1 || return 1
+    *) usage "$0" 1
        ;;
   esac
 done
@@ -40,13 +40,15 @@ done
 test -z "${VERSION}" && VERSION="${DEFAULT_VERSION}"
 echo "Downloading VERSION:${VERSION}"
 
-export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
-export OS=$(uname | awk '{print tolower($0)}')
+ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n "$(uname -m)" ;; esac)
+export ARCH
+OS=$(uname | awk '{print tolower($0)}')
+export OS
 export OPERATOR_SDK_DL_URL="https://github.com/operator-framework/operator-sdk/releases/download/v${VERSION}"
-curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
+curl -LO "${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}"
 gpg --keyserver keyserver.ubuntu.com --recv-keys 052996E2A20B5C7E
-curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt
-curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt.asc
+curl -LO "${OPERATOR_SDK_DL_URL}/checksums.txt"
+curl -LO "${OPERATOR_SDK_DL_URL}/checksums.txt.asc"
 gpg -u "Operator SDK (release) <cncf-operator-sdk@cncf.io>" --verify checksums.txt.asc
-grep operator-sdk_${OS}_${ARCH} checksums.txt | sha256sum -c -
-#chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+grep "operator-sdk_${OS}_${ARCH}" checksums.txt | sha256sum -c -
+chmod +x "operator-sdk_${OS}_${ARCH}" && sudo mv "operator-sdk_${OS}_${ARCH}" /usr/local/bin/operator-sdk
